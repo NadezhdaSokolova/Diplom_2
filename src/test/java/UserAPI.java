78cc11b9e08aa6337f5f993633baa836d332f0f2
Nadezhda Sokolova <Nadegda1V@yandex.ru>
2024-03-27 16:41:26 +0300

    UserCreationTest + added info in gitignore + API classes created

---
+++ src/test/java/UserAPI.java
@@ -0,0 +1,49 @@
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+
+public class UserAPI {
+
+    final static String USER = "/api/auth/register";
+    final static String USERDATA = "/api/auth/user";
+    final static String USERLOGIN = "/api/auth/login";
+
+
+    @Step("Make post-request to create user")
+    public static Response createUser(UserPOJO user) {
+
+        Response response = given()
+                .header("Content-type", "application/json")
+                .body(user)
+                .post(USER);
+
+        return response;
+
+    }
+
+    @Step("Make post-request to login of courier")
+    public static Response autorizedUser(UserPOJO user){
+
+        Response response = given()
+                .header("Content-type", "application/json")
+                .body(user)
+                .post(USERLOGIN);
+
+        return response;
+
+    }
+
+    @Step("Make delete-request to delete of courier")
+    public static Response deleteUser(UserPOJO user){
+
+        Response response = given()
+                .header("Content-type", "application/json")
+                .body(user)
+                .delete(USERDATA);
+
+        return response;
+
+    }
+
+
+}
--- src/test/java/UserCreationTest.java
+++ src/test/java/UserCreationTest.java
@@ -1,12 +1,100 @@
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class UserCreationTest {
 
+    String email = "Nadezhda42" + Math.random() + "@yandex.ru";
+    String password = "qwerty1232" + Math.random();
+
+
     @Before
     public void setUp() {
-        RestAssured.baseURI = "https://stellarburgers.nomoreparties.site/api/auth/register";
+        RestAssured.baseURI = "https://stellarburgers.nomoreparties.site/";
+
+        // создаем тестового пользователя
+        UserPOJO user1 = new UserPOJO("Nadezhda5@yandex.ru", "111111", "Надежда");
+
+        String token = UserAPI.createUser(user1).header("Authorization");
+        String[] split = token.split(" ");
+        String tokenNumber = split[1].substring(0, split[1].length() - 1);
+        System.out.println(tokenNumber);
+    }
+
+    @After
+    public void deleteTestUser(){
+        UserPOJO user1 = new UserPOJO("Nadezhda5@yandex.ru", "111111", "Надежда");
+
+        try  {
+
+            String token = UserAPI.autorizedUser(user1).header("Authorization");
+            String[] split = token.split(" ");
+            String tokenNumber = split[1].substring(0, split[1].length() - 1);
+            System.out.println(tokenNumber);
+
+            UserAPI.deleteUser(user1);
+        }
+        catch (Exception e){
+            System.out.println ("Удалять нечего. Пользователь не прошел авторизацию.");
+        }
+
     }
 
 
+    @Test
+    public void uniqueSuccessUserCreation() {
+        UserPOJO user = new UserPOJO(email, password, "Надежда");
+
+        UserAPI.createUser(user).then().statusCode(200)
+                .and()
+                .body("success", equalTo(true));
+
+    }
+
+    @Test
+    public void AttemptUserCreationWithoutRequiredNameField() {
+        UserPOJO user = new UserPOJO(email, password, null);
+
+        UserAPI.createUser(user).then().statusCode(403)
+                .and()
+                .body("message", equalTo("Email, password and name are required fields"));
+    }
+
+
+    @Test
+    public void AttemptUserCreationWithoutRequiredPasswordField() {
+        UserPOJO user = new UserPOJO(email, null, "Надежда");
+
+        UserAPI.createUser(user).then().statusCode(403)
+                .and()
+                .body("message", equalTo("Email, password and name are required fields"));
+    }
+
+    @Test
+    public void AttemptUserCreationWithoutRequiredEmailField() {
+        UserPOJO user = new UserPOJO(null, password, "Надежда");
+
+        UserAPI.createUser(user).then().statusCode(403)
+                .and()
+                .body("message", equalTo("Email, password and name are required fields"));
+    }
+
+
+    @Test
+    public void AttemptAlreadyExistedUserCreation() {
+
+        // сперва пользователя нужно создать в предусловии
+        UserPOJO user = new UserPOJO("Nadezhda5@yandex.ru", "111111", "Надежда");
+
+        UserAPI.createUser(user).then().statusCode(403)
+                .and()
+                .body("message", equalTo("User already exists"));
+    }
+
 }
+
